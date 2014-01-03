b2c6fb3e0a907288f22942d344ec4bfeff55da04
liujh <liujh@nationalchip.com>
2014-01-03 21:00:04 +0800

    add popen, pclose exameple

---
+++ src/proc_convert.c
@@ -0,0 +1,30 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+	char		line[MAXLINE];
+	FILE		*fpin;
+	
+	if((fpin = popen("./bin/tool_convert","r")) == NULL){
+		err_sys("popen error");
+	}
+
+	for(;;){
+		fputs("Prompt> ", stdout);
+		fflush(stdout);
+		if(fgets(line, MAXLINE, fpin) == NULL) {//read from pipe
+			break;
+		}
+		if(fputs(line, stdout) == EOF){
+			err_sys("fputs error to pipe");
+		}
+	}
+
+	if(pclose(fpin) == -1){
+		err_sys("pclose error");
+	}
+
+	putchar('\n');
+	exit(0);
+}
